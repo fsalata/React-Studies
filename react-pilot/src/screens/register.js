52e0f5a41cec17bed860223d4e6f47d70d651cf4
Fábio Salata <fabio.salata@dartdigital.com.br>
2018-04-24 16:31:40 -0300

    Criação cadastro

--- react-pilot/src/screens/register.js
+++ react-pilot/src/screens/register.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+
+import TextInput from '../components/textInput';
+import Modal from '../components/modal';
 
 class Register extends Component {
   render() {
