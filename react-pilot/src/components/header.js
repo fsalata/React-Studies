a6649cd55db859adf8c6e340caf211c5cd46ad82
Fábio Salata <fabio.salata@dartdigital.com.br>
2018-05-02 13:28:15 -0300

    Ajustes Header

--- react-pilot/src/components/header.js
+++ react-pilot/src/components/header.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 
-const Header = () => (
-  <nav className="navbar navbar-dark bg-dark fixed-top">
-    <div className="container">
-      <Link to="/" className="navbar-brand">
-        React Pilot
-      </Link>
-    </div>
-  </nav>
-);
+import { getUserProfile } from '../actions/userProfile';
 
-export default Header;
+import Avatar from './avatar';
+
+class Header extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (
+      nextProps.email &&
+      nextProps.userProfile.email === null &&
+      nextProps.userProfile.isLoading === null
+    ) {
+      this.props.getUserProfile(nextProps.email);
+    }
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark fixed-top">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            React Pilot
+          </Link>
+
+          {this.props.isloggedIn ? (
+            <Avatar size="35" image={this.props.userProfile.profileImage} iconSize="2x" />
+          ) : null}
+        </div>
+      </nav>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string,
+  userProfile: PropTypes.object,
+  isloggedIn: PropTypes.bool,
+  getUserProfile: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+  userProfile: state.userProfile,
+  isloggedIn: state.loggedUser.status,
+  email: state.loggedUser.email,
+});
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      getUserProfile,
+    },
+    dispatch,
+  );
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- react-pilot/src/sagas/loggedUserSaga.js
+++ react-pilot/src/sagas/loggedUserSaga.js
@@ -1,15 +1,15 @@
-import LocalForage from "localforage";
-import { takeEvery, put } from "redux-saga/effects";
+import LocalForage from 'localforage';
+import { takeEvery, put } from 'redux-saga/effects';
 
 import {
   GET_LOGIN_STATUS,
   SET_LOGIN_STATUS,
-  SET_LOGIN_STATUS_SUCCESS
-} from "../actions/loggedUser";
+  SET_LOGIN_STATUS_SUCCESS,
+} from '../actions/loggedUser';
 
 function* getLoginStatus() {
   try {
-    let user = yield LocalForage.getItem("loggedUser").then(storedUser => {
+    let user = yield LocalForage.getItem('loggedUser').then((storedUser) => {
       if (storedUser) {
         return JSON.parse(storedUser);
       }
@@ -20,7 +20,7 @@ function* getLoginStatus() {
       user = {
         email: null,
         password: null,
-        status: false
+        status: false,
       };
     }
 
@@ -32,10 +32,7 @@ function* getLoginStatus() {
 
 function* setLoginStatus({ user }) {
   try {
-    const storedUser = yield LocalForage.setItem(
-      "loggedUser",
-      JSON.stringify(user)
-    ).then(savedUser => JSON.parse(savedUser));
+    const storedUser = yield LocalForage.setItem('loggedUser', JSON.stringify(user)).then(savedUser => JSON.parse(savedUser));
 
     if (user) {
       yield put({ type: SET_LOGIN_STATUS_SUCCESS, loggedUser: storedUser });
@@ -47,7 +44,7 @@ function* setLoginStatus({ user }) {
 
 export const loggedUserSaga = [
   takeEvery(GET_LOGIN_STATUS, getLoginStatus),
-  takeEvery(SET_LOGIN_STATUS, setLoginStatus)
+  takeEvery(SET_LOGIN_STATUS, setLoginStatus),
 ];
 
 export default loggedUserSaga;
